0ac36789fbd1e1b93f244bfc05bad52b4b1c6fbd
marina601 <marina601@live.com>
2021-08-03 13:35:08 +0000

    Customized Admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,33 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Creating columns for each data
+    field in Products collection
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Creating columns for each data
+    field in Category collection
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,14 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        """
+        Specify verbose name or plural name
+        from django defaults
+        """
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, blank=True)
 
@@ -17,13 +25,13 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=False, blank=True)
+    sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=False, blank=True)
+    image_url = models.URLField(max_length=1024, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):

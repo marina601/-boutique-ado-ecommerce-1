7f6871209f6d7b7a51d877bd38c423d106af01a9
marina601 <marina601@live.com>
2021-08-06 09:15:23 +0000

    Updated context processor

--- bag/views.py
+++ bag/views.py
@@ -16,14 +16,22 @@ def add_to_bag(request, item_id):
     """
 
     quantity = int(request.POST.get('quantity'))
+    # from the form redirect input
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    """
+    Add item to the bag or update the quantity
+    if its already exists
+    """
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
 
+    """
+    Overwrite the variable in the session with
+    the updated version
+    """
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)

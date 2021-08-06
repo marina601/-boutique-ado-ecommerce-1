ab79b28214d8c31d33ec9ca70677bc02b57e1043
marina601 <marina601@live.com>
2021-08-06 07:41:45 +0000

    Added bag context and delivery login

---
+++ bag/contexts.py
@@ -0,0 +1,42 @@
+from decimal import Decimal
+from django.conf import settings
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    """
+    If the total is less than delivery threshold
+    multiply the total by 10%
+    """
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        # calculates how much more the customer needs to spend in order
+        # to qualify for free delivery
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    # Calculate the grand total of the purchase
+    grand_total = delivery + total
+
+    """
+    Adding all the items to the context
+    makes them available as variables across
+    all the templates
+    """
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context

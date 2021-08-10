1924c95f5351ac512ec65840921072a7bf675ec3
marina601 <marina601@live.com>
2021-08-10 09:28:51 +0000

    Added adjusted bag and remove the bag from views, and subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,27 +17,27 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
+            """
+            Checking if the item_data is integer
+            that means it is a quantity
+            """
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
-            item_total = item_data * product.price
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
-                'item_total': item_total,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
-                item_total = quantity * product.price
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
-                    'item_total': item_total,
                     'size': size,
                 })
 
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -25,6 +25,10 @@ def add_to_bag(request, item_id):
 
     if size:
         if item_id in list(bag.keys()):
+            """
+            Check if another item of th same id
+            and same size already exists and increment by 1
+            """
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
             else:
@@ -33,6 +37,7 @@ def add_to_bag(request, item_id):
             bag[item_id] = {'items_by_size': {size: quantity}}
     else:
         """
+        If there is no size
         Add item to the bag or update the quantity
         if its already exists
         """
@@ -47,3 +52,57 @@ def add_to_bag(request, item_id):
     """
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """
+    Adjust the quantity of the specific product
+    to the specific amount
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """
+    Remove the items from the shopping bag
+    """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500, e=e)

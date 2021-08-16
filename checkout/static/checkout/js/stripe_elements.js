5928ab893e52bb9b45538b2ca99971b885e4f611
marina601 <marina601@live.com>
2021-08-16 09:21:22 +0000

    added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,23 @@
+const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+const client_secret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripe_public_key);
+const elements = stripe.elements();
+
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvatica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+const card = elements.create('card', {style: style});
+card.mount('#card_elements');

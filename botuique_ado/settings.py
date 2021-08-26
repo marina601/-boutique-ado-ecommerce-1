27cc78c5ce1dd32c552a5b60e55aa271b576163f
marina601 <marina601@live.com>
2021-08-26 10:42:19 +0000

    Changed the value of the secret key

--- botuique_ado/settings.py
+++ botuique_ado/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env.str('SECRET_KEY', 'sample_unsafe_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ

a3603b36f6cfe1c5dce9ae4ab624c8405f7c1bde
marina601 <marina601@live.com>
2021-08-30 10:37:41 +0000

    Removed env.py file

--- botuique_ado/settings.py
+++ botuique_ado/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.exists("env.py"):
-    import env
+# if os.path.exists("env.py"):
+  #  import env
 
 # env = environ.Env()
 # reading .env file

869e5ec250a9c50e716ecf5ba84e665287fc2d0c
marina601 <marina601@live.com>
2021-08-30 10:14:41 +0000

    Fix a typo in staticfiles location

--- botuique_ado/settings.py
+++ botuique_ado/settings.py
@@ -206,7 +206,7 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATIC_STORAGE = 'custom_storages.StaticStorage'
-    STATIC_LOCATION  = 'static'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 

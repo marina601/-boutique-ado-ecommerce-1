519bfab7f3b8bebf78c2be7483d83127b0eefc8c
marina601 <marina601@live.com>
2021-08-30 13:40:36 +0000

    Configured gmail to send out real emails

--- botuique_ado/settings.py
+++ botuique_ado/settings.py
@@ -116,7 +116,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 # allwes authentication via username or email
@@ -242,4 +242,14 @@ STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
 # STRIPE_PUBLIC_KEY = env("STRIPE_PUBLIC_KEY")
 # STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 # STRIPE_WH_SECRET = env("STRIPE_WH_SECRET")
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
